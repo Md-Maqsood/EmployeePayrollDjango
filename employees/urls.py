9174dbd2fa8e77d626ca3a1337af273ce721d3a6
maqsood <maqsoodhamd37@gmail.com>
2021-01-02 14:34:19 +0530

    [Maqsood].update: adde the feature to edit employee details

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import employeesList, addEmployee
+from .views import employeesList, addEmployee, editEmployee
 
 urlpatterns=[
     path('',employeesList),
     path('add/',addEmployee),
+    path('edit/<int:pk>/',editEmployee),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Employee, Department
-from datetime import datetime
+from datetime import datetime, timedelta
 # Create your views here.
 def employeesList(request):
     employees=Employee.objects.all()
@@ -27,4 +27,34 @@ def addEmployee(request):
         employee.save()
         return HttpResponseRedirect('/employees/')
     else:
-        return render(request,'Employee_Form.html')
+        dateToday=datetime.now()
+        dateMonthBefore=dateToday-timedelta(days=30)
+        return render(request,'Employee_Form.html',{'minDate':dateMonthBefore.date(),'maxDate':dateToday.date()})
+
+def editEmployee(request,pk):
+    employee=Employee.objects.get(id=pk)
+    if request.method=='POST':
+        employee.name=request.POST['name']
+        employee.gender=request.POST['gender']
+        employee.salary=int(request.POST['salary'])
+        employee.profile_pic=request.POST['profile_pic']
+        employee.note=request.POST['note']
+        employee.start_date=datetime.strptime(request.POST['start_date'], '%Y-%m-%d').date()
+        departmentNames=request.POST.getlist('departments')
+        initialDepartments=employee.departments.all()
+        for departmentName in departmentNames:
+            try:
+                department=Department.objects.get(department_name=departmentName)
+            except Department.DoesNotExist:
+                department=Department(department_name=departmentName)
+                department.save()
+            employee.departments.add(department)
+        for department in initialDepartments:
+            if department.department_name not in departmentNames:
+                employee.departments.remove(department)
+        employee.save()
+        return HttpResponseRedirect('/employees/')
+    else:
+        dateToday=datetime.now()
+        dateMonthBefore=dateToday-timedelta(days=30)
+        return render(request,'Employee_Form.html',{'minDate':dateMonthBefore.date(),'maxDate':dateToday.date(),'employee':employee})
